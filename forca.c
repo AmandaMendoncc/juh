9c112f8d5bb2e480bd2bce7ca6d76fbd4a6ffd9c
AmandaMendoncc <87458987+AmandaMendoncc@users.noreply.github.com>
2024-06-22 22:26:13 -0300

    Update forca.c

--- forca.c
+++ forca.c
@@ -8,13 +8,18 @@
 void Forca()
 {
 	int ganhou = 0;
-while (1) {
+	
+	while (1) {
+		
         FILE *arquivo;
         arquivo = fopen("palavras.txt", "r");
-        if (arquivo == NULL) {
-            perror("Erro ao abrir o arquivo de palavras");
+        
+        if (arquivo == NULL)
+		{
+            perror("\n\n\n\t\t\tERRO AO ABRIR O ARQUIVO!");
             return;
         }
+
 
         char palavra[100];
         char palavra_digitada[100]; // Array para armazenar a palavra secreta digitada
@@ -27,19 +32,25 @@ while (1) {
         int min = 0;
         int max = 631;
         int linha_sorteada = min + rand() % (max - min + 1);
-
         int contador_de_linhas = 0;
         
-        while (fgets(palavra, sizeof(palavra), arquivo) != NULL) {
+        
+        
+        while (fgets(palavra, sizeof(palavra), arquivo) != NULL)
+		{
             contador_de_linhas++;
-            if (contador_de_linhas == linha_sorteada) {
+            if (contador_de_linhas == linha_sorteada)
+			{
                 break;
             }
         }
         fclose(arquivo);
 
+
+
         // Remover o caractere de nova linha (\n) da palavra lida
         palavra[strcspn(palavra, "\n")] = '\0';
+
 
 		Limpar();
 		Logo();
@@ -55,13 +66,16 @@ while (1) {
             int verificador = 0;
 
             if (tentativas_erradas < 10) {
+            	
                 Buffer();
-                printf("\n\n\n\t\t\tDIGITE UMA LETRA: ");
+                printf("\n\n\n\n\t\t\tDIGITE UMA LETRA: ");
                 scanf(" %c", &letra);
 
+
                 // Verificar se a letra já foi digitada
-                if (letras_digitadas[letra - 'a'] == 1 || letras_digitadas[letra - 'A'] == 1) {
-                    printf("Você já digitou essa letra. Tente outra.\n");
+                if (letras_digitadas[letra - 'a'] == 1 || letras_digitadas[letra - 'A'] == 1)
+				{
+                    printf("\n\t\t\tVOCÊ JÁ DIGITOU ESSA LETRA! DIGITE OUTRA. ");
                     continue;
                 }
 
@@ -79,6 +93,7 @@ while (1) {
                 }
 
                 // mostra a palavra
+                printf("\n\t\t\t");
                 for (i = 0; i < strlen(palavra); i++) {
                     if (palavra_digitada[i] != ' ') {
                         printf(" %c ", palavra_digitada[i]);
@@ -89,36 +104,29 @@ while (1) {
                 printf("\n");
 
                 if (verificador != 0) {
-                    printf("Essa letra existe na palavra\n");
+                    printf("\n\t\t\tESSA LETRA EXISTE NA PALAVRA.");
                 } else {
-                    printf("Essa letra não existe na palavra\n");
+                    printf("\n\t\t\tESSA LETRA NÃO EXISTE NA PALAVRA.");
                     tentativas_erradas++;
                 }
 
-                printf("Tentativas restantes: %d\n", 10 - tentativas_erradas);
+                printf("\n\t\t\tTENTATIVAS RESTANTES: %d", 10 - tentativas_erradas);
             } else {
-                printf("Máximo de erros atingido. Você foi enforcado!\n");
+            	Limpar();
+            	Logo();
+                printf("\n\n\t\t\tMÁXIMO DE ERROS ATINGIDO.");
                 ganhou = 1;
                 break;
             }
         }
         
         if(ganhou == 1){
-        	printf("Voce perdeu!!!!!\n");
-        	printf("A palavra era %s\n", palavra);
+        	printf("\n\t\t\tVOCÊ FOI ENFORCADO! MAIS ATENÇÃO NA PRÓXIMA.");
+        	printf("\n\t\t\tA PALAVRE ERA: %s", palavra);
 		} else {
-			printf("PARABENS VOCE GANHOU!!!!!!!!!!!!!!");
+			printf("\n\t\t\tPARABÉNS VOCE GANHOU!");
 		}
-
-        // deseja jogar novamente
-        char jogar_novamente;
-        Buffer(); // função para o scanf nao pular
-        printf("Deseja jogar novamente? (s/n): ");
-        scanf(" %c", &jogar_novamente);
-
-        if (jogar_novamente != 's' && jogar_novamente != 'S') {
-            MenuPrincipal();
-            break;
-        }
+		
+		JogarNovamente();
     }
 }
