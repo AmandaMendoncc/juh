ca2bb59da0b55afef59571dcc975e9a5fc8d33d9
AmandaMendoncc <87458987+AmandaMendoncc@users.noreply.github.com>
2024-06-23 18:36:53 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -15,8 +15,7 @@ void MenuPrincipal()
         printf("\n\n\n\t[1] - JOGO DA VELHA");
         printf("\n\t[2] - CAMPO MINADO");
         printf("\n\t[3] - JOGO DA FORCA");
-        printf("\n\t[4] - RANKING");
-        printf("\n\t[5] - SAIR DO PROGRAMA");
+        printf("\n\t[4] - SAIR DO PROGRAMA");
         printf("\n\n\tESCOLHA UMA OPÇÃO: ");
         scanf("%d", &opcao);
 
@@ -27,15 +26,12 @@ void MenuPrincipal()
                 Velha(nome);
                 break;
             case 2:
-                // CHAMAR FUNÇÃO CAMPO MINADO
+            	CampoMinado();
                 break;
             case 3:
                 Forca();
                 break;
             case 4:
-                // CHAMAR FUNÇÃO RANKING
-                break;
-            case 5:
                 printf("\n\tSAINDO...");
                 break;
             default:
